f167389234129c0828f00b7cd093b8da61b562ea
Denis Lebedev <3d.lebedzeu@gmail.com>
2025-11-28 21:04:05 +0300

    Add universal acl

--- lebedev_lab3/archive_manager.py
+++ lebedev_lab3/archive_manager.py
@@ -5,13 +5,11 @@ import tarfile
 import tempfile
 from collections.abc import Iterator
 from pathlib import Path
-from typing import Dict, Union
+from typing import Any, Dict, List, Optional
 
-# Определяем платформу
 IS_WINDOWS = platform.system() == "Windows"
 IS_LINUX = platform.system() == "Linux"
 
-# Импортируем соответствующие модули в зависимости от платформы
 if IS_WINDOWS:
     import pythonnet
     pythonnet.load("coreclr")
@@ -25,128 +23,159 @@ if IS_WINDOWS:
 if IS_LINUX:
     from linux_acl_handler import LinuxACL, LinuxAclHandler
 
-# Универсальная обертка для работы с ACL
-class AclHandler:
-    @staticmethod
-    def GetFileAcl(file_path: str):
-        if IS_WINDOWS:
-            return CSharpAclHandler.GetFileAcl(file_path)
-        elif IS_LINUX:
-            return LinuxAclHandler.get_file_acl(file_path)
-        else:
-            raise NotImplementedError(f"Platform {platform.system()} is not supported")
+
+class UniversalACL:
+    def __init__(self):
+        self.platform: str = platform.system()  # "Windows" или "Linux"
+        self.data: Dict[str, Any] = {}
+    
+    def to_dict(self) -> dict:
+        return {
+            "platform": self.platform,
+            "data": self.data
+        }
+    
+    def to_json(self) -> str:
+        return json.dumps(self.to_dict(), indent=2)
     
     @staticmethod
-    def GetDirAcl(dir_path: str):
-        if IS_WINDOWS:
-            return CSharpAclHandler.GetDirAcl(dir_path)
-        elif IS_LINUX:
-            return LinuxAclHandler.get_dir_acl(dir_path)
-        else:
-            raise NotImplementedError(f"Platform {platform.system()} is not supported")
+    def from_dict(data: dict) -> "UniversalACL":
+        acl = UniversalACL()
+        acl.platform = data.get("platform", "Unknown")
+        acl.data = data.get("data", {})
+        return acl
     
     @staticmethod
-    def SetFileAcl(file_path: str, acl):
-        if IS_WINDOWS:
-            return CSharpAclHandler.SetFileAcl(file_path, acl)
-        elif IS_LINUX:
-            return LinuxAclHandler.set_file_acl(file_path, acl)
-        else:
-            raise NotImplementedError(f"Platform {platform.system()} is not supported")
+    def from_json(json_str: str) -> "UniversalACL":
+        data = json.loads(json_str)
+        return UniversalACL.from_dict(data)
     
     @staticmethod
-    def SetDirAcl(dir_path: str, acl):
-        if IS_WINDOWS:
-            return CSharpAclHandler.SetDirAcl(dir_path, acl)
-        elif IS_LINUX:
-            return LinuxAclHandler.set_dir_acl(dir_path, acl)
-        else:
-            raise NotImplementedError(f"Platform {platform.system()} is not supported")
+    def from_windows_acl(csharp_acl) -> "UniversalACL":
+        acl = UniversalACL()
+        acl.platform = "Windows"
+        acl.data = json.loads(csharp_acl.ToJson())
+        return acl
+    
+    @staticmethod
+    def from_linux_acl(linux_acl: 'LinuxACL') -> "UniversalACL":
+        acl = UniversalACL()
+        acl.platform = "Linux"
+        acl.data = linux_acl.to_dict()
+        return acl
+    
+    def to_windows_acl(self):
+        if not IS_WINDOWS:
+            raise RuntimeError("Cannot create Windows ACL on non-Windows platform")
+        json_str = json.dumps(self.data)
+        return CSharpACL.FromJson(json_str)
+    
+    def to_linux_acl(self) -> 'LinuxACL':
+        if not IS_LINUX:
+            raise RuntimeError("Cannot create Linux ACL on non-Linux platform")
+        return LinuxACL.from_dict(self.data)
+    
+    def merge(self, other: "UniversalACL"):
+        if self.platform != other.platform:
+            raise ValueError("Cannot merge ACLs from different platforms")
+        
+        if self.platform == "Windows":
+            if not self.data.get("Owner"):
+                self.data["Owner"] = other.data["Owner"]
+            
+            if not self.data.get("Group"):
+                self.data["Group"] = other.data["Group"]
+            
+            # Merge Aces
+            if "Aces" not in self.data:
+                self.data["Aces"] = {}
+            
+            for sid, ace_list in other.data.get("Aces", {}).items():
+                if sid not in self.data["Aces"]:
+                    self.data["Aces"][sid] = []
+                
+                for ace in ace_list:
+                    if ace in self.data["Aces"][sid]:
+                        self.data["Aces"][sid].remove(ace)
+                    self.data["Aces"][sid].append(ace)
+            
+            # Merge Saces
+            if "Saces" not in self.data:
+                self.data["Saces"] = {}
+            
+            for sid, ace_list in other.data.get("Saces", {}).items():
+                if sid not in self.data["Saces"]:
+                    self.data["Saces"][sid] = []
+                
+                for ace in ace_list:
+                    if ace in self.data["Saces"][sid]:
+                        self.data["Saces"][sid].remove(ace)
+                    self.data["Saces"][sid].append(ace)
+        
+        elif self.platform == "Linux":
+            if not self.data.get("owner"):
+                self.data["owner"] = other.data["owner"]
+            
+            if not self.data.get("group"):
+                self.data["group"] = other.data["group"]
+            
+            if not self.data.get("mode"):
+                self.data["mode"] = other.data["mode"]
+            
+            # Merge acl_entries
+            if "acl_entries" not in self.data:
+                self.data["acl_entries"] = []
+            
+            for entry in other.data.get("acl_entries", []):
+                if entry in self.data["acl_entries"]:
+                    self.data["acl_entries"].remove(entry)
+                self.data["acl_entries"].append(entry)
 
 
-class PathAcl(Dict[str, Union['CSharpACL', 'LinuxACL']]):
+class PathAcl(Dict[str, UniversalACL]):
     def __init__(self):
         super().__init__()
 
-    def __getitem__(self, key: str):
+    def __getitem__(self, key: str) -> UniversalACL:
         return super().__getitem__(key)
 
-    def __setitem__(self, key: str, value):
+    def __setitem__(self, key: str, value: UniversalACL):
         if key in self:
-            acl = super().__getitem__(key)
-            # Merge для C# ACL
-            if IS_WINDOWS and hasattr(acl, 'Merge'):
-                acl.Merge(value)
-                super().__setitem__(key, acl)
-            # Merge для Linux ACL
-            elif IS_LINUX and hasattr(acl, 'merge'):
-                acl.merge(value)
-                super().__setitem__(key, acl)
-            else:
-                super().__setitem__(key, value)
+            existing_acl = super().__getitem__(key)
+            existing_acl.merge(value)
+            super().__setitem__(key, existing_acl)
         else:
             super().__setitem__(key, value)
-
-    def __items__(self):
-        for key in self:
-            acl = self[key]
-            if IS_WINDOWS and hasattr(acl, 'Aces'):
-                yield key, [acl.Aces[ace_key] for ace_key in acl.Aces.Keys]
-            else:
-                yield key, acl
 
 
 class SystemAclsDict(Dict[str, PathAcl]):
     def __init__(self):
         super().__init__()
-        self.system = platform.system()
-        super().__setitem__(self.system, PathAcl())
+        self.current_system = platform.system()
 
-    def __getitem__(self, key: str):
-        system_map = super().__getitem__(self.system)
-        return system_map.get(key)
+    def add_acl(self, system_name: str, file_path: str, acl: UniversalACL):
+        if system_name not in self:
+            super().__setitem__(system_name, PathAcl())
+        
+        self[system_name][file_path] = acl
 
-    def __setitem__(self, key: str, value):
-        if self.system not in self:
-            super().__setitem__(self.system, PathAcl())
+    def get_acl(self, file_path: str) -> Optional[UniversalACL]:
+        if self.current_system in self:
+            return self[self.current_system].get(file_path)
+        return None
 
-        system_map = super().__getitem__(self.system)
-        system_map[key] = value
-
-    def __len__(self) -> int:
-        if self.system not in self:
-            return 0
-        return len(super().__getitem__(self.system))
-
-    def __iter__(self) -> Iterator[str]:
-        if self.system not in self:
-            return iter(())
-
-        return iter(super().__getitem__(self.system))
-
-    def keys(self):
-        return super().__getitem__(self.system).keys()
-
-    def items(self):
-        return super().__getitem__(self.system).items()
-
-    def values(self):
-        return super().__getitem__(self.system).values()
+    def get_current_system_acls(self) -> PathAcl:
+        if self.current_system not in self:
+            super().__setitem__(self.current_system, PathAcl())
+        return self[self.current_system]
 
     def to_json(self) -> str:
         json_dict = {}
-        for system_name, files in super().items():
+        for system_name, path_acls in super().items():
             json_dict[system_name] = {}
-            for file_name, acl in files.items():
-                # Для Windows используем C# метод ToJson
-                if IS_WINDOWS and hasattr(acl, 'ToJson'):
-                    json_dict[system_name][file_name] = acl.ToJson()
-                # Для Linux используем Python метод to_json
-                elif IS_LINUX and hasattr(acl, 'to_json'):
-                    json_dict[system_name][file_name] = acl.to_json()
-                else:
-                    json_dict[system_name][file_name] = json.dumps(acl)
-
+            for file_path, universal_acl in path_acls.items():
+                json_dict[system_name][file_path] = universal_acl.to_dict()
+        
         return json.dumps(json_dict, indent=4)
     
     @staticmethod
@@ -155,41 +184,87 @@ class SystemAclsDict(Dict[str, PathAcl]):
         json_dict = json.loads(json_str)
 
         for system_name, files in json_dict.items():
-            super(SystemAclsDict, acls).__setitem__(system_name, PathAcl())
-            for file_name, acl_json in files.items():
-                # Определяем тип ACL по имени системы
-                if system_name == "Windows" and IS_WINDOWS:
-                    acl = CSharpACL.FromJson(acl_json)
-                elif system_name == "Linux" and IS_LINUX:
-                    from linux_acl_handler import LinuxACL
-                    acl = LinuxACL.from_json(acl_json)
-                else:
-                    # Пропускаем ACL для других систем
-                    continue
-                
-                super(SystemAclsDict, acls).__getitem__(system_name)[file_name] = acl
+            for file_path, acl_data in files.items():
+                universal_acl = UniversalACL.from_dict(acl_data)
+                acls.add_acl(system_name, file_path, universal_acl)
 
         return acls
     
     @staticmethod
-    def load_from_dir(dir: Path) -> "SystemAclsDict":
+    def load_from_source(p: Path) -> "SystemAclsDict":
         acls = SystemAclsDict()
 
-        for acls_file in dir.rglob(".acls"):
-            rel_path = acls_file.relative_to(dir).parent
+        if p.is_dir():
+            if (p.parent / ".acls").exists():
+                with open(p.parent / ".acls", "r", encoding="utf-8") as f:
+                    json_str = f.read()
+
+                inner = SystemAclsDict.from_json(json_str)
+                for system_name, path_acls in super(SystemAclsDict, inner).items():
+                    for file_path, universal_acl in path_acls.items():
+                        full_path = str(p / file_path)
+                        acls.add_acl(system_name, full_path, universal_acl)
+        else:
+            p = p.parent
+
+        for acls_file in p.rglob(".acls"):
+            rel_path = acls_file.relative_to(p).parent
             with open(acls_file, "r", encoding="utf-8") as f:
                 json_str = f.read()
 
             inner = SystemAclsDict.from_json(json_str)
-            for system_name, files in super(SystemAclsDict, inner).items():
-                if system_name not in acls:
-                    super(SystemAclsDict, acls).__setitem__(system_name, {})
-
-                system_dict = super(SystemAclsDict, acls).__getitem__(system_name)
-                for file_name, acl in files.items():
-                    system_dict[str(rel_path / file_name)] = acl
+            for system_name, path_acls in super(SystemAclsDict, inner).items():
+                for file_path, universal_acl in path_acls.items():
+                    full_path = str(rel_path / file_path)
+                    acls.add_acl(system_name, full_path, universal_acl)
 
         return acls
+
+
+class AclHandler:
+    @staticmethod
+    def GetFileAcl(file_path: str) -> UniversalACL:
+        if IS_WINDOWS:
+            csharp_acl = CSharpAclHandler.GetFileAcl(file_path)
+            return UniversalACL.from_windows_acl(csharp_acl)
+        elif IS_LINUX:
+            linux_acl = LinuxAclHandler.get_file_acl(file_path)
+            return UniversalACL.from_linux_acl(linux_acl)
+        else:
+            raise NotImplementedError(f"Platform {platform.system()} is not supported")
+    
+    @staticmethod
+    def GetDirAcl(dir_path: str) -> UniversalACL:
+        if IS_WINDOWS:
+            csharp_acl = CSharpAclHandler.GetDirAcl(dir_path)
+            return UniversalACL.from_windows_acl(csharp_acl)
+        elif IS_LINUX:
+            linux_acl = LinuxAclHandler.get_dir_acl(dir_path)
+            return UniversalACL.from_linux_acl(linux_acl)
+        else:
+            raise NotImplementedError(f"Platform {platform.system()} is not supported")
+    
+    @staticmethod
+    def SetFileAcl(file_path: str, universal_acl: UniversalACL):
+        if IS_WINDOWS:
+            csharp_acl = universal_acl.to_windows_acl()
+            CSharpAclHandler.SetFileAcl(file_path, csharp_acl)
+        elif IS_LINUX:
+            linux_acl = universal_acl.to_linux_acl()
+            LinuxAclHandler.set_file_acl(file_path, linux_acl)
+        else:
+            raise NotImplementedError(f"Platform {platform.system()} is not supported")
+    
+    @staticmethod
+    def SetDirAcl(dir_path: str, universal_acl: UniversalACL):
+        if IS_WINDOWS:
+            csharp_acl = universal_acl.to_windows_acl()
+            CSharpAclHandler.SetDirAcl(dir_path, csharp_acl)
+        elif IS_LINUX:
+            linux_acl = universal_acl.to_linux_acl()
+            LinuxAclHandler.set_dir_acl(dir_path, linux_acl)
+        else:
+            raise NotImplementedError(f"Platform {platform.system()} is not supported")
 
 
 def is_empty_dir(path: Path) -> bool:
@@ -207,15 +282,17 @@ class ArchiveManager:
         items = [p for p in [source_path] + list(source_path.rglob("*")) if p.name != ".acls"]
         total_items = len(items)
         
-        acls = SystemAclsDict.load_from_dir(parent)
+        acls = SystemAclsDict.load_from_source(source_path)
 
         with tempfile.TemporaryDirectory() as tmp_dir_name:
             tmp_acl_dir = Path(tmp_dir_name)
 
             with tarfile.open(archive_path, "w") as tar:
                 for idx, item in enumerate(items, 1):
-                    acl = AclHandler.GetDirAcl(str(item)) if item.is_dir() else AclHandler.GetFileAcl(str(item))
-                    acls[str(item.relative_to(parent))] = acl
+                    universal_acl = AclHandler.GetDirAcl(str(item)) if item.is_dir() else AclHandler.GetFileAcl(str(item))
+                    
+                    rel_path = str(item.relative_to(parent))
+                    acls.add_acl(acls.current_system, rel_path, universal_acl)
 
                     if item.is_file() or is_empty_dir(item):
                         tar.add(item, arcname=item.relative_to(parent))
@@ -247,14 +324,16 @@ class ArchiveManager:
             acl_json_str = f.read()
 
         acls = SystemAclsDict.from_json(acl_json_str)
-        total_entries = len(acls)
+        current_system_acls = acls.get_current_system_acls()
+        total_entries = len(current_system_acls)
 
-        for idx, (file_path, acl) in enumerate(acls.items(), 1):
-            file_path = output_dir / file_path
-            if file_path.is_file():
-                AclHandler.SetFileAcl(str(file_path), acl)
-            elif file_path.is_dir():
-                AclHandler.SetDirAcl(str(file_path), acl)
+        for idx, (file_path, universal_acl) in enumerate(current_system_acls.items(), 1):
+            full_path = output_dir / file_path
+            
+            if full_path.is_file():
+                AclHandler.SetFileAcl(str(full_path), universal_acl)
+            elif full_path.is_dir():
+                AclHandler.SetDirAcl(str(full_path), universal_acl)
 
             if progress_callback:
                 progress_callback(idx, total_entries, f"Unpacking entry {idx}/{total_entries}")
